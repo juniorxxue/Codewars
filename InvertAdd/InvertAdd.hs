5d6b9543b56732728d9ed4930acac334b7c0fb8a
Xu Xue <juniorxxue@gmail.com>
2021-06-25 14:39:56 +0800

    try to understand computation

--- InvertAdd/InvertAdd.hs
+++ InvertAdd/InvertAdd.hs
@@ -25,9 +25,28 @@ type instance S n :+: m = S (n :+: m)
 
 refl :: Natural n -> Equal n n
 refl NumZ = EqlZ
-refl (NumS n) = EqlS (refl n) 
+refl (NumS n) = EqlS (refl n)
+
+symm :: Equal a b -> Equal b a
+symm EqlZ = EqlZ
+symm (EqlS e) = EqlS (symm e)
+
+trans :: Equal a b -> Equal b c -> Equal a c
+trans EqlZ EqlZ = EqlZ
+trans (EqlS e1) (EqlS e2) = EqlS (trans e1 e2)
+
+pred :: Equal (S a) (S b) -> Equal a b
+pred (EqlS e) = e
+
+add :: Natural a -> Natural b -> Equal (a :+: S b) (S (a :+: b))
+-- Equal (S b) (S b)
+add NumZ b = refl (NumS b)
+-- Equal (S a :+: S b) (S (S a :+: b))
+-- Equal (S (a :+: S b)) (S S (a :+: b))
+-- Equal (a :+: S b)
+add (NumS a) b = EqlS (add a b)
 
 -- do some computation
 
 invert :: Natural a -> Natural b -> Equal (a :+: a) (b :+: b) -> Equal a b
-invert NumZ NumZ (EqlS EqlZ) = EqlZ
+invert NumZ NumZ EqlZ = EqlZ
